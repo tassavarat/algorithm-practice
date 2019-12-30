214b3e976fafc89d2cfbca516f6b4ff0578862a1
Tim Assavarat <tassavarat@gmail.com>
2019-12-30 15:08:36 -0800

    Changed indentation

--- linked_lists/remove_k_from_list.c
+++ linked_lists/remove_k_from_list.c
@@ -7,27 +7,27 @@
  */
 list_integer *remove_k_from_list(list_integer *l, int k)
 {
-    list_integer *cur, *tmp;
+	list_integer *cur, *tmp;
 
-    cur = tmp = l;
+	cur = tmp = l;
 
-    while (cur)
-    {
-        if (cur->value == k && cur != l)
-        {
-            tmp->next = cur->next;
-            free(cur);
-        }
-        else
-        {
-            tmp = cur;
-        }
-        cur = tmp->next;
-        if (tmp->value == k)
-        {
-            l = cur;
-            free(tmp);
-        }
-    }
-    return (l);
+	while (cur)
+	{
+		if (cur->value == k && cur != l)
+		{
+			tmp->next = cur->next;
+			free(cur);
+		}
+		else
+		{
+			tmp = cur;
+		}
+		cur = tmp->next;
+		if (tmp->value == k)
+		{
+			l = cur;
+			free(tmp);
+		}
+	}
+	return (l);
 }
