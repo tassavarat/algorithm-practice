1867c9e315ff6ed36f0902dcf25016a7d9c85af2
Tim Assavarat <tassavarat@gmail.com>
2020-01-13 23:50:47 -0800

    Adding header file

---
+++ trees/trees.h
@@ -0,0 +1,38 @@
+#ifndef TREES_H
+#define TREES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * struct tree_integer - binary tree node
+ *
+ * @value: integer stored in node
+ * @left: pointer to left child node
+ * @right: pointer to right child node
+ */
+struct tree_integer {
+	int value;
+	struct tree_integer *left;
+	struct tree_integer *right;
+};
+
+
+/**
+ * struct stack - doubly linked list
+ * @sum: current sum
+ * @node: pointer to binary tree node
+ * @prev: pointer to previous node
+ * @next: pointer to next node
+ */
+struct stack {
+	int sum;
+	struct tree_integer *node;
+	struct stack *prev;
+	struct stack *next;
+};
+
+bool has_path_with_given_sum(struct tree_integer *t, int s);
+
+#endif /* TREES_H */
