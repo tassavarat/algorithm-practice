3af714c1a4d09223cbe3797e90daced067c630fa
Tim Assavarat <tassavarat@gmail.com>
2020-01-16 18:11:44 -0800

    Adding comments

--- trees/has_path_with_given_sum_recursive.c
+++ trees/has_path_with_given_sum_recursive.c
@@ -1,5 +1,12 @@
 #include "trees.h"
 
+/**
+ * has_path_with_given_sum - recursive DFS of tree while checking for given sum
+ *
+ * Description:
+ * sum is value of all nodes along path from root to leaf node
+ * Return: true if sum found otherwise false
+ */
 bool has_path_with_given_sum(struct tree_integer *t, int s)
 {
 	if (!t)
@@ -7,5 +14,6 @@ bool has_path_with_given_sum(struct tree_integer *t, int s)
 	s -= t->value;
 	if (!t->left && !t->right)
 		return (s == 0);
-	return (has_path_with_given_sum(t->left, s) || has_path_with_given_sum(t->right, s));
+	return (has_path_with_given_sum(t->left, s) ||
+			has_path_with_given_sum(t->right, s));
 }
