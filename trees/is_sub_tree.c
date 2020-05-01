63771cfb44f8cddf8a3c6d49302882e61e1d7b1c
tassavarat <tassavarat@gmail.com>
2020-04-30 17:55:01 -0700

    Check sub tree solution

---
+++ trees/is_sub_tree.c
@@ -0,0 +1,67 @@
+#include "trees.h"
+
+bool check_subtree(tree_integer *t1, tree_integer *t2)
+{
+	if (!t2)
+		return t1 ? 0 : 1;
+	if (!t1)
+		return 0;
+	if (t1->value != t2->value)
+		return 0;
+	if (!check_subtree(t1->left, t2->left) ||
+			!check_subtree(t1->right, t2->right))
+		return 0;
+	return 1;
+}
+
+bool is_subtree(tree_integer *t1, tree_integer *t2)
+{
+	if (!t1 && !t2)
+		return 1;
+	if (!t1)
+		return 0;
+	if (check_subtree(t1, t2))
+		return 1;
+	if (is_subtree(t1->left, t2) || is_subtree(t1->right, t2))
+		return 1;
+	return 0;
+}
+
+tree_integer *create_node(int value)
+{
+	tree_integer *new = malloc(sizeof(*new));
+
+	if (!new)
+		return NULL;
+	new->value = value;
+	new->left = NULL;
+	new->right = NULL;
+	return new;
+}
+
+int main()
+{
+	tree_integer *t1, *t2;
+
+	t1 = create_node(5);
+	t1->left = create_node(10);
+	t1->right = create_node(7);
+	t1->left->left = create_node(4);
+	t1->left->right = create_node(6);
+	t1->left->left->left = create_node(1);
+	t1->left->left->right = create_node(2);
+	t1->left->right->right = create_node(-1);
+	print_t(t1);
+	t2 = create_node(10);
+	t2->left = create_node(4);
+	t2->right = create_node(6);
+	t2->left->left = create_node(1);
+	t2->left->right = create_node(2);
+	t2->right->right = create_node(-1);
+	print_t(t2);
+	if (is_subtree(t1, t2))
+		puts("t2 is a subtree of t1");
+	else
+		puts("t2 is not a subtree of t1");
+	return 0;
+}
